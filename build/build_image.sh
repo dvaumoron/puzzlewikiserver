308a40e09aa22bf9c15220911780bb9974732438
dvaumoron <dvaumoron@gmail.com>
2023-03-15 17:40:30 +0100

    add build_image.sh

---
+++ build/build_image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+go install
+
+buildah from --name puzzlewikiserver-working-container scratch
+buildah copy puzzlewikiserver-working-container $HOME/go/bin/puzzlewikiserver /bin/puzzlewikiserver
+buildah config --env SERVICE_PORT=50051 puzzlewikiserver-working-container
+buildah config --port 50051 puzzlewikiserver-working-container
+buildah config --entrypoint '["/bin/puzzlewikiserver"]' puzzlewikiserver-working-container
+buildah commit puzzlewikiserver-working-container puzzlewikiserver
+buildah rm puzzlewikiserver-working-container
